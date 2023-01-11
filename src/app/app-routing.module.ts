42b64c98c26a22e4f92f2553d590b2b9dec5aef0
jimmy cruz <cruzperezjimmya@gmail.com>
2023-01-11 10:02:40 -0500

    se añade componentes, rutas y se estructura el header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // se establece las rutas para navegacion de componentes
+  {path:'', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'**', pathMatch:'full', redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
